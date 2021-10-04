b031d44dce84bdc84e498bf817689a94613b6c71
evan <evan.hermier@gmail.com>
2021-10-05 00:51:32 +0200

    init project + add schema photo

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const customEnv = require("./customEnv");
+
+const app = express();
+
+mongoose
+  .connect(customEnv.mongoConnect, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+module.exports = app;
---
+++ backend/models/Photo.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const photoSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  photoUrl: { type: String, required: true },
+  userID: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Photo", photoSchema);
